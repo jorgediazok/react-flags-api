bcb9fcc10ea0b03730dfdf6f070fa2140f270209
Jorge <jorgediazok@gmail.com>
2021-04-19 03:00:57 -0300

    created filter component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Filter from './components/Filter';
 import Countries from './components/Countries';
 
 function App() {
   return (
     <>
       <Header />
+      <Filter />
       <Countries />
     </>
   );
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 const Filter = () => {
-  return <div></div>;
+  return (
+    <section className="filter">
+      <form className="form-control">
+        <input
+          type="search"
+          name="search"
+          id="search"
+          placeholder="Search for a country"
+        />
+      </form>
+
+      <div>
+        <select name="select" id="select" className="select">
+          <option value="Filter by region">Filter by Region</option>
+          <option value="Africa">Africa</option>
+          <option value="America">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </div>
+    </section>
+  );
 };
 
 export default Filter;
