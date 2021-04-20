ade0378f4874701e78faa55f79fd39ee7f95e3dd
Jorge <jorgediazok@gmail.com>
2021-04-20 02:56:47 -0300

    country page styled

--- src/components/Country.js
+++ src/components/Country.js
@@ -11,18 +11,88 @@ const Country = () => {
       const response = await fetch(
         `https://restcountries.eu/rest/v2/name/${name}`
       );
-      const country = await response.json();
-      setCountry(country);
-      console.log(country);
+      const countryData = await response.json();
+      setCountry(countryData);
+      console.log(countryData);
     };
     fetchCountryData();
   }, [name]);
 
   return (
     <>
-      <Link to="/" className="btn btn-light">
-        <i className="fas fa-arrow-left">Back Home</i>
-      </Link>
+      <section className="country">
+        <Link to="/" className="btn btn-light">
+          <i className="fas fa-arrow-left"> Back Home</i>
+        </Link>
+        {country.map((ctry) => {
+          const {
+            numericCode,
+            flag,
+            name,
+            nativeName,
+            population,
+            region,
+            subregion,
+            capital,
+            topLevelDomain,
+            currencies,
+            languages,
+            borders,
+          } = ctry;
+          return (
+            <article key={numericCode}>
+              <div className="country-inner">
+                <div className="flag">
+                  <img src={flag} alt={name} />
+                </div>
+                <div className="country-details">
+                  <div>
+                    <h2>{name}</h2>
+                    <h5>
+                      Native Name: <span>{nativeName}</span>
+                    </h5>
+                    <h5>
+                      Population: <span>{population}</span>
+                    </h5>
+                    <h5>
+                      Region: <span>{region}</span>
+                    </h5>
+                    <h5>
+                      Sub Region: <span>{subregion}</span>
+                    </h5>
+                    <h5>
+                      Capital: <span>{capital}</span>
+                    </h5>
+                  </div>
+                  <div>
+                    <h5>
+                      Top Level Domain: <span>{topLevelDomain}</span>
+                    </h5>
+                    <h5>
+                      Currencies: <span>{currencies[0].name}</span>
+                    </h5>
+                    <h5>
+                      Languages: <span>{languages[0].name}</span>
+                    </h5>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <h3>Borders:</h3>
+                <div className="borders">
+                  {borders.map((border) => {
+                    return (
+                      <ul key={border}>
+                        <li>{border}</li>
+                      </ul>
+                    );
+                  })}
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </section>
     </>
   );
 };
